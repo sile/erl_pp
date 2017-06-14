f5c6453c6ea0db1e704ca24eb266e5d19aa79df1
Takeru Ohta <phjgt308@gmail.com>
2017-06-15 04:19:58 +0900

    Update documentation

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ An Erlang source code preprocessor written in Rust.
 References
 ----------
 
-- [Erlang Reference Manual](http://erlang.org/doc/reference_manual/macros.html)
+- [Erlang Reference Manual -- Preprocessor](http://erlang.org/doc/reference_manual/macros.html)
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,29 @@
+//! Erlang source code preprocessor.
+//!
+//! # Examples
+//!
+//! ```
+//! # extern crate erl_pp;
+//! # extern crate erl_tokenize;
+//! use erl_pp::Preprocessor;
+//! use erl_tokenize::Lexer;
+//!
+//! # fn main() {
+//! let src = r#"-define(FOO(A), {A, ?LINE}). io:format("Hello: ~p", [?FOO(bar)])."#;
+//! let pp = Preprocessor::new(Lexer::new(src));
+//! let tokens = pp.collect::<Result<Vec<_>, _>>().unwrap();
+//!
+//! assert_eq!(tokens.iter().map(|t| t.text()).collect::<Vec<_>>(),
+//!            ["io", ":", "format", "(", r#""Hello: ~p""#, ",",
+//!             "[", "{", "bar", ",", "1", "}", "]", ")", "."]);
+//! # }
+//! ```
+//!
+//! # References
+//!
+//! - [Erlang Reference Manual -- Preprocessor](http://erlang.org/doc/reference_manual/macros.html)
+//!
+#![warn(missing_docs)]
 extern crate erl_tokenize;
 extern crate glob;
 #[macro_use]
