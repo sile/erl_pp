ff7ee2a605f08cfc2b18917e7c32ee8d45a78483
Takeru Ohta <phjgt308@gmail.com>
2017-06-18 18:02:03 +0900

    Update pp.rs

--- examples/pp.rs
+++ examples/pp.rs
@@ -4,21 +4,38 @@ extern crate erl_tokenize;
 #[macro_use]
 extern crate trackable;
 
+use std::env;
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
 use std::time::{Duration, Instant};
 use clap::{App, Arg};
-use erl_pp::Preprocessor;
-use erl_tokenize::{Lexer, PositionRange};
+use erl_pp::{Preprocessor, MacroDef};
+use erl_tokenize::{Lexer, PositionRange, Position};
+use erl_tokenize::tokens::AtomToken;
+use trackable::error::{ErrorKindExt, Failed};
 
 fn main() {
     let matches = App::new("pp")
         .arg(Arg::with_name("SOURCE_FILE").index(1).required(true))
         .arg(Arg::with_name("SILENT").long("silent"))
+        .arg(
+            Arg::with_name("CURRENT_DIR")
+                .long("current-dir")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("ERL_LIBS")
+                .long("libs")
+                .takes_value(true)
+                .multiple(true),
+        )
         .get_matches();
     let src_file = Path::new(matches.value_of("SOURCE_FILE").unwrap());
     let silent = matches.is_present("SILENT");
+    if let Some(dir) = matches.value_of("CURRENT_DIR") {
+        track_try_unwrap!(env::set_current_dir(dir).map_err(|e| Failed.cause(e)));
+    }
 
     let mut src = String::new();
     let mut file = File::open(&src_file).expect("Cannot open file");
@@ -30,7 +47,22 @@ fn main() {
     let mut lexer = Lexer::new(&src);
     lexer.set_filepath(src_file.file_name().unwrap());
 
-    let preprocessor = Preprocessor::new(lexer);
+    let mut preprocessor = Preprocessor::new(lexer);
+    if let Some(libs) = matches.values_of("ERL_LIBS") {
+        for dir in libs {
+            preprocessor.code_paths_mut().push_back(dir.into());
+        }
+    }
+    preprocessor.macros_mut().insert(
+        "MODULE".to_string(),
+        MacroDef::Dynamic(vec![
+            AtomToken::from_value(
+                src_file.file_stem().unwrap().to_str().unwrap(),
+                Position::new()
+            ).into(),
+        ]),
+    );
+
     for result in preprocessor {
         let token = track_try_unwrap!(result);
         if !silent {
